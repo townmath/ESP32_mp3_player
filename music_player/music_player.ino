ff03bfb07d3afa1470ec73bcd2bf1a8eb73a15a4
Jim Town <townmath@users.noreply.github.com>
2025-03-14 06:22:59 -0700

    Update music_player.ino
    Plays next song after first song is over.

--- music_player/music_player.ino
+++ music_player/music_player.ino
@@ -1,19 +1,31 @@
-//esp32 Dev
+//trying ESP32 Wrover Module with defaults and Huge App, works!
+//can leave bottom plugged in as long as you hold the volume knob towards the headphone jack while uploading
+
+//settings:
+// board=esp32 Dev
 // regular defaults except:
 // partition scheme Huge APP
-// also you have eto close and reopen arduino every time you want to recompile
+// also you have to close and reopen arduino every time you want to recompile,yes or changing the board works too
 //Jim Updates:
 //default volume = calm 7 instead of blaring 21
 //sort files
+//when song over, play next one
+//
+//Can't write to sd card? xremember last song played (saves index in a text file whenever music is paused)
 //TODO: fast forward?
 //TODO: remember last song played?
-
+const bool isDebug = false;//true;//
 #include "Arduino.h"
-//#include "WiFiMulti.h"
+
+//Audio
 #include "Audio.h"
 #include "SPI.h"
+
+//SD Card
 #include "SD.h"
 #include "FS.h"
+
+//LCD
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
@@ -28,7 +40,7 @@
 #define I2S_BCLK 26
 #define I2S_LRC 25
 
-//SSD1306
+//SSD1306 aka LCD
 #define MAKEPYTHON_ESP32_SDA 4
 #define MAKEPYTHON_ESP32_SCL 5
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
@@ -48,21 +60,26 @@ const int Pin_next = 2;
 
 
 const int DEFAULTVOL=7;//21
-const int MAXSONGS=30;
+const int MAXSONGS=20;
+
+void logoshow();
+void open_new_song(String filename);
+void sort_music_list(int lastFile,String fileList[MAXSONGS]);
+int get_music_list(fs::FS &fs, const char *dirname, uint8_t levels, String wavlist[MAXSONGS], int & file_index);
+void print_song_time();
+void print_song_info();
+void lcd_text(String text);
+void display_music();
+void open_new_song(String filename);
+//const String lastSongFileName = "lastSongFile.txt";//can't save?
 
 Audio audio;
-
-/*
-WiFiMulti wifiMulti;
-String ssid = "Makerfabs";
-String password = "20160704";
-*/
 
 struct Music_info
 {
     String name;
-    int length;
-    int runtime;
+    uint32_t length;
+    uint32_t runtime;
     int volume;
     int status;
     int mute_volume;
@@ -71,9 +88,11 @@ struct Music_info
 String file_list[MAXSONGS];
 int file_num = 0;
 int file_index = 0; //first song is 0th song
+bool isPaused=false;
+uint run_time = 0;
+uint button_time = 0;
 
-void setup()
-{
+void setup() {
     //IO mode init
     pinMode(Pin_vol_up, INPUT_PULLUP);
     pinMode(Pin_vol_down, INPUT_PULLUP);
@@ -83,16 +102,17 @@ void setup()
     pinMode(Pin_next, INPUT_PULLUP);
 
     //Serial
-    Serial.begin(115200);
+    if (isDebug)
+      Serial.begin(115200);
 
     //LCD
     Wire.begin(MAKEPYTHON_ESP32_SDA, MAKEPYTHON_ESP32_SCL);
     // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
-    { // Address 0x3C for 128x32
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+      if (isDebug)
         Serial.println(F("SSD1306 allocation failed"));
-        for (;;)
-            ; // Don't proceed, loop forever
+      for (;;)
+          ; // Don't proceed, loop forever
     }
     display.clearDisplay();
     logoshow();
@@ -102,44 +122,32 @@ void setup()
     digitalWrite(SD_CS, HIGH);
     SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
     SPI.setFrequency(1000000);
-    if (!SD.begin(SD_CS, SPI))
-    {
-        Serial.println("Card Mount Failed");
+    if (!SD.begin(SD_CS, SPI)) {
+        if (isDebug) Serial.println("Card Mount Failed");
         lcd_text("SD ERR");
         while (1)
             ;
     }
-    else
-    {
+    else {
         lcd_text("SD OK");
     }
 
     //Read SD
-    file_num = get_music_list(SD, "/", 0, file_list);
-    /*for (int i = 0; i < file_num; i++) //debugging
+    file_num = get_music_list(SD, "/", 0, file_list, file_index);
+    for (int i = 0; i < file_num; i++) //debugging
     {
         Serial.println(file_list[i]);
-    }*/
+    }
+    
     sort_music_list(file_num,file_list);
-    Serial.print("Music file count:");
-    Serial.println(file_num);
-    Serial.println("Sorted music:");
-    for (int i = 0; i < file_num; i++)
-    {
+    if (isDebug){
+      Serial.print("Music file count:");
+      Serial.println(file_num);
+      Serial.println("Sorted music:");
+      for (int i = 0; i < file_num; i++) {
         Serial.println(file_list[i]);
+      }
     }
-
-    //WiFi
-    /*
-    WiFi.mode(WIFI_STA);
-    wifiMulti.addAP(ssid.c_str(), password.c_str());
-    wifiMulti.run();
-    if (WiFi.status() != WL_CONNECTED)
-    {
-        WiFi.disconnect(true);
-        wifiMulti.run();
-    }
-    */
 
     //Audio(I2S)
     audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
@@ -149,27 +157,41 @@ void setup()
     //file_list[0] = "MoonlightBay.mp3";
     open_new_song(file_list[file_index]);
     print_song_time();
+    print_song_info();
+    run_time = millis();
+    
 }
-
-uint run_time = 0;
-uint button_time = 0;
 
 void loop()
 {
+  
     audio.loop();
-    if (millis() - run_time > 1000)
-    {
+    if (millis() - run_time > 1000) {
         run_time = millis();
         print_song_time();
         display_music();
+        if (music_info.length<=music_info.runtime) { //if song over play next
+          audio.stopSong();
+          if (file_index < file_num - 1)
+              file_index++;
+          else
+              file_index = 0;
+          open_new_song(file_list[file_index]);
+          print_song_info();
+        }
     }
 
-    if (millis() - button_time > 300)
-    {
+    if (millis() - button_time > 300) {
         //Button logic
-        if (digitalRead(Pin_next) == 0)
-        {
-            Serial.println("Pin_next");
+        if (digitalRead(Pin_next) == 0) {
+            if (isDebug) Serial.println("Pin_next");
+            if (isPaused){
+                uint32_t filePos=audio.getFilePos();
+                if (isDebug){
+                  Serial.print(filePos);
+                  Serial.println(" position of the file at the current time");
+                }
+            }
             if (file_index < file_num - 1)
                 file_index++;
             else
@@ -178,9 +200,8 @@ void loop()
             print_song_time();
             button_time = millis();
         }
-        if (digitalRead(Pin_previous) == 0)
-        {
-            Serial.println("Pin_previous");
+        if (digitalRead(Pin_previous) == 0) {
+            //if (isDebug) Serial.println("Pin_previous");
             if (file_index > 0)
                 file_index--;
             else
@@ -191,7 +212,7 @@ void loop()
         }
         if (digitalRead(Pin_vol_up) == 0)
         {
-            Serial.println("Pin_vol_up");
+            //if (isDebug) Serial.println("Pin_vol_up");
             if (music_info.volume < 21)
                 music_info.volume++;
             audio.setVolume(music_info.volume);
@@ -199,7 +220,7 @@ void loop()
         }
         if (digitalRead(Pin_vol_down) == 0)
         {
-            Serial.println("Pin_vol_down");
+            //if (isDebug) Serial.println("Pin_vol_down");
             if (music_info.volume > 0)
                 music_info.volume--;
             audio.setVolume(music_info.volume);
@@ -207,7 +228,7 @@ void loop()
         }
         if (digitalRead(Pin_mute) == 0)
         {
-            Serial.println("Pin_mute");
+            //if (isDebug) Serial.println("Pin_mute");
             if (music_info.volume != 0)
             {
                 music_info.mute_volume = music_info.volume;
@@ -222,40 +243,28 @@ void loop()
         }
         if (digitalRead(Pin_pause) == 0)
         {
-            Serial.println("Pin_pause");
+            //if (isDebug) Serial.println("Pin_pause");
             audio.pauseResume();
             button_time = millis();
+            isPaused=!isPaused;
         }
     }
+   
 
-    //串口控制切歌，音量
-    if (Serial.available())
-    {
-        String r = Serial.readString();
-        r.trim();
-        if (r.length() > 5)
-        {
-            audio.stopSong();
-            open_new_song(file_list[0]);
-            print_song_info();
-        }
-        else
-        {
-            audio.setVolume(r.toInt());
-        }
-    }
+    
+    
 }
 
 void open_new_song(String filename)
 {
-    //去掉文件名的根目录"/"和文件后缀".mp3",".wav"
     music_info.name = filename.substring(0, filename.indexOf("."));
-    audio.connecttoFS(SD, filename);
+    audio.connecttoFS(SD, filename.c_str());
     music_info.runtime = audio.getAudioCurrentTime();
     music_info.length = audio.getAudioFileDuration();
+    //music_info.length=  audio.getFileSize()  / (audio.getSampleRate() * audio.getChannels());
     music_info.volume = audio.getVolume();
     music_info.status = 1;
-    Serial.println("**********start a new sound************");
+    //Serial.println("**********start a new sound************");
 }
 
 void display_music()
@@ -292,7 +301,7 @@ void display_music()
     display.display();
 }
 
-void logoshow(void)
+void logoshow()
 {
     display.clearDisplay();
 
@@ -303,7 +312,7 @@ void logoshow(void)
     display.setCursor(0, 10); // Start at top-left corner
     display.println(F("Music"));
     display.setCursor(0, 20); // Start at top-left corner
-    display.println(F("Player V2.1"));
+    display.println(F("Player V2.2"));
     display.setCursor(0,30);
     display.println(F("Jim Town Edition"));
     display.display();
@@ -324,64 +333,73 @@ void lcd_text(String text)
 
 void print_song_info()
 {
+  
+  if (isDebug){
     Serial.println("***********************************");
+    Serial.print("File Size:");
     Serial.println(audio.getFileSize());
+    Serial.print("File Pos:");
     Serial.println(audio.getFilePos());
+    Serial.print("Sample Rate:");
     Serial.println(audio.getSampleRate());
+    Serial.print("Bits Per Sample:");
     Serial.println(audio.getBitsPerSample());
+    Serial.print("Channels:");
     Serial.println(audio.getChannels());
+    Serial.print("Volume:");
     Serial.println(audio.getVolume());
     Serial.println("***********************************");
+  }
+  
 }
 
-//刷新歌曲时间
 void print_song_time()
 {
-    //Serial.println(audio.getAudioCurrentTime());
-    //Serial.println(audio.getAudioFileDuration());
+  if (isDebug){
+    Serial.print("Current Time:");
+    Serial.println(audio.getAudioCurrentTime());
+    Serial.print("Total Time:");
+    Serial.println(audio.getAudioFileDuration());
+  }
     music_info.runtime = audio.getAudioCurrentTime();
     music_info.length = audio.getAudioFileDuration();
     music_info.volume = audio.getVolume();
 }
 
-int get_music_list(fs::FS &fs, const char *dirname, uint8_t levels, String wavlist[MAXSONGS])
+int get_music_list(fs::FS &fs, const char *dirname, uint8_t levels, String wavlist[MAXSONGS], int & file_index)
 {
-    Serial.printf("Listing directory: %s\n", dirname);
+    if (isDebug) Serial.printf("Listing directory: %s\n", dirname);
     int i = 0;
 
     File root = fs.open(dirname);
-    if (!root)
-    {
-        Serial.println("Failed to open directory");
+    
+    if (!root){
+        if (isDebug) Serial.println("Failed to open directory");
         return i;
     }
-    if (!root.isDirectory())
-    {
-        Serial.println("Not a directory");
+    if (!root.isDirectory()){
+        if (isDebug) Serial.println("Not a directory");
         return i;
     }
 
     File file = root.openNextFile();
-    while (file)
-    {
-        if (file.isDirectory())
-        {
-        }
-        else
-        {
+    while (file && i<MAXSONGS){
+        if (!file.isDirectory()){
             String temp = file.name();
-            if (temp.endsWith(".wav"))
-            {
+            if (temp.endsWith(".wav")){
                 wavlist[i] = temp;
                 i++;
             }
-            else if (temp.endsWith(".mp3"))
-            {
+            else if (temp.endsWith(".mp3")){
                 wavlist[i] = temp;
+                if (isDebug) Serial.println(temp);//testing
                 i++;
-            }
+            } 
         }
         file = root.openNextFile();
+    }
+    if (isDebug && i>=MAXSONGS){
+      Serial.println("i>=MAXSONGS!!");
     }
     return i;
 }
@@ -401,70 +419,4 @@ void sort_music_list(int lastFile,String fileList[MAXSONGS]){
     }    
   } while (swapped);
 
-}
-
-//**********************************************
-// optional
-void audio_info(const char *info)
-{
-    Serial.print("info        ");
-    Serial.println(info);
-}
-void audio_id3data(const char *info)
-{ //id3 metadata
-    Serial.print("id3data     ");
-    Serial.println(info);
-}
-
-//歌曲结束逻辑
-void audio_eof_mp3(const char *info)
-{ //end of file
-    Serial.print("eof_mp3     ");
-    Serial.println(info);
-    file_index++;
-    if (file_index >= file_num)
-    {
-        file_index = 0;
-    }
-    open_new_song(file_list[file_index]);
-}
-void audio_showstation(const char *info)
-{
-    Serial.print("station     ");
-    Serial.println(info);
-}
-void audio_showstreaminfo(const char *info)
-{
-    Serial.print("streaminfo  ");
-    Serial.println(info);
-}
-void audio_showstreamtitle(const char *info)
-{
-    Serial.print("streamtitle ");
-    Serial.println(info);
-}
-void audio_bitrate(const char *info)
-{
-    Serial.print("bitrate     ");
-    Serial.println(info);
-}
-void audio_commercial(const char *info)
-{ //duration in sec
-    Serial.print("commercial  ");
-    Serial.println(info);
-}
-void audio_icyurl(const char *info)
-{ //homepage
-    Serial.print("icyurl      ");
-    Serial.println(info);
-}
-void audio_lasthost(const char *info)
-{ //stream URL played
-    Serial.print("lasthost    ");
-    Serial.println(info);
-}
-void audio_eof_speech(const char *info)
-{
-    Serial.print("eof_speech  ");
-    Serial.println(info);
 }
